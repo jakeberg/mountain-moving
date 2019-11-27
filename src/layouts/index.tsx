bc8e64fead06c7a982d9383c260fa703ffcb8b1b
Jake Berg <jacobvictorberg@gmail.com>
2019-11-27 13:24:04 -0500

    pages are now functional components

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,70 +1,38 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from "react";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
-interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
+interface DefaultLayoutProps {
   location: {
-    pathname: string
-  }
-  children: any
+    pathname: string;
+  };
+  children: any;
 }
 
-class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
-  public render() {
-    return (
-      <div>
-        <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {this.props.children()}
-        </div>
+const Layout: React.FC<DefaultLayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Helmet
+        title="Mountain Moving Company"
+        meta={[
+          { name: "description", content: "Sample" },
+          { name: "keywords", content: "sample, something" }
+        ]}
+      />
+      appbar
+      <div
+        style={{
+          margin: "0 auto",
+          maxWidth: 960,
+          padding: "0px 1.0875rem 1.45rem",
+          paddingTop: 0
+        }}
+      >
+        {children()}
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default DefaultLayout
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
+import * as React from "react";
+import Link from "gatsby-link";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -7,30 +7,24 @@ interface IndexPageProps {
   data: {
     site: {
       siteMetadata: {
-        title: string
-      }
-    }
-  }
+        title: string;
+      };
+    };
+  };
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
-}
+const Homepage: React.FC<IndexPageProps> = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        <strong>{props.data.site.siteMetadata.title}</strong> site.
+      </p>
+      asdf
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+  );
+};
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -40,4 +34,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
+
+export default Homepage;
